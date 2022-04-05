67d40377266d88b454e56fbd90d69b64ec2bb32c
ea-open-source <open-source@eastagile.com>
2022-04-05 08:38:34 +0700

    [f] 931 - Add oauth
    [#181708931] https://www.pivotaltracker.com/story/show/181708931

--- src/accounts/views/login.rs
+++ src/accounts/views/login.rs
@@ -1,7 +1,12 @@
-use jelly::actix_web::{web::Form, HttpRequest};
+use jelly::actix_session::UserSession;
+use jelly::actix_web::{web::Form, HttpRequest, web};
+use jelly::actix_web::web::head;
 use jelly::prelude::*;
 use jelly::request::{Authentication, DatabasePool};
 use jelly::Result;
+use oauth2::basic::BasicClient;
+use oauth2::{CsrfToken, Scope};
+use oauth2::http::header;
 
 use crate::accounts::forms::LoginForm;
 use crate::accounts::Account;
@@ -49,3 +54,26 @@ pub async fn authenticate(request: HttpRequest, form: Form<LoginForm>) -> Result
         context
     })
 }
+
+pub async fn oauth(request: HttpRequest, client: web::Data<BasicClient>) -> Result<HttpResponse> {
+    if request.is_authenticated()? {
+        return request.redirect("/dashboard/");
+    }
+
+    let (authorize_url, csrf_state) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("public_repo".to_string()))
+        .add_scope(Scope::new("user:email".to_string()))
+        .url();
+
+    info!(
+        "Open this URL in your browser:\n{}\n",
+        authorize_url.to_string()
+    );
+    request.get_session().set("state", &csrf_state);
+    Ok(HttpResponse::Found()
+        .header(header::LOCATION, authorize_url.to_string())
+        .finish()
+    )
+}
+
--- tests/features/step_definitions/signup_steps.rs
+++ tests/features/step_definitions/signup_steps.rs
@@ -1,4 +1,4 @@
-use cucumber::{given, when, then};
+use cucumber::{when, then};
 use thirtyfour::prelude::*;
 
 use super::super::world::TestWorld;
@@ -8,13 +8,10 @@ async fn click_on_sign_up_button(world: &mut TestWorld) {
     signup_button.click().await.unwrap();
 }
 
-#[then("I should see the sign up page")]
-async fn see_sign_up_page(world: &mut TestWorld) {
-  assert_eq!(world.driver.current_url().await.unwrap(), "http://localhost:17002/accounts/register/");
-}
-
 #[when("I fill in my email and password and submit the form on the sign up page")]
 async fn fill_in_sign_up_form(world: &mut TestWorld) {
+    std::fs::remove_dir_all("./emails").unwrap();
+
     let email_field = world.driver.find_element(By::Name("email")).await.unwrap();
     email_field.send_keys("test@email.com").await.unwrap();
 
@@ -24,7 +21,7 @@ async fn fill_in_sign_up_form(world: &mut TestWorld) {
     create_account_button.click().await.unwrap();
 }
 
-#[when(regex = r"^I fill in a valid email with value of '([\w@.]+)' and an invalid password with value of ([\w@$!%*#.]+)$")]
+#[when(expr = "I fill in a valid email with value of '{word}' and an invalid password with value of {word}")]
 async fn fill_in_invalid_password(world: &mut TestWorld, email: String, invalid_password: String) {
     let email_field = world.driver.find_element(By::Name("email")).await.unwrap();
     email_field.send_keys(email).await.unwrap();
@@ -35,15 +32,9 @@ async fn fill_in_invalid_password(world: &mut TestWorld, email: String, invalid_
     create_account_button.click().await.unwrap();
 }
 
-#[when(regex = r"^I fill in email with value of ([\w@]+) and valid password$")]
-async fn fill_in_invalid_email(world: &mut TestWorld, invalid_email: String) {
-    let email_field = world.driver.find_element(By::Name("email")).await.unwrap();
-    email_field.send_keys(&invalid_email).await.unwrap();
-
-    let password_field = world.driver.find_element(By::Name("password")).await.unwrap();
-    password_field.send_keys("SuperStr0ngP@").await.unwrap();
-    let create_account_button = world.driver.find_element(By::ClassName("create_account_btn")).await.unwrap();
-    create_account_button.click().await.unwrap();
+#[then("I should see the sign up page")]
+async fn see_sign_up_page(world: &mut TestWorld) {
+  assert_eq!(world.driver.current_url().await.unwrap(), "http://localhost:17002/accounts/register/");
 }
 
 #[then("I should see that my account has been created")]
@@ -51,7 +42,6 @@ async fn see_my_account_created(world: &mut TestWorld) {
     let heading = world.driver.find_element(By::Tag("h1")).await.unwrap();
     let heading_text = heading.text().await.unwrap();
     assert_eq!(heading_text, "Verify Account");
-    world.close_browser().await;
 }
 
 #[then(regex = r"^I should see the error '([\w\s?]+)'$")]
@@ -59,5 +49,14 @@ async fn see_error_message(world: &mut TestWorld, message: String) {
     let errors_element = world.driver.find_element(By::ClassName("error")).await.unwrap();
     let errors_message = errors_element.text().await.unwrap();
     assert!(errors_message.contains(&message));
+    world.close_browser().await;
+}
+
+#[then("I should receive a verification email")]
+async fn receive_verification_email(world: &mut TestWorld) {
+    let path = std::fs::read_dir("./emails").unwrap().next();
+    let contents = std::fs::read_to_string(path.unwrap().unwrap().path()).unwrap();
+    assert!(contents.contains("test@email.com"));
+    assert!(contents.contains("Verify your new account"));
     world.close_browser().await;
 }
