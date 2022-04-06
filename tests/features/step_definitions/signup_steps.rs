0fd64dacc5d81b45c6e75785a31e1a0892e90b77
ea-open-source <open-source@eastagile.com>
2022-04-06 07:53:24 +0700

    [r] 931 - Add missing context and refactor
    [#181708931] https://www.pivotaltracker.com/story/show/181708931

--- tests/features/step_definitions/signup_steps.rs
+++ tests/features/step_definitions/signup_steps.rs
@@ -1,10 +1,14 @@
-use cucumber::{when, then};
+use cucumber::{then, when};
 use thirtyfour::prelude::*;
 
 use super::super::world::TestWorld;
 #[when("I click on the Sign up button on the home page")]
 async fn click_on_sign_up_button(world: &mut TestWorld) {
-    let signup_button = world.driver.find_element(By::ClassName("sign-up")).await.unwrap();
+    let signup_button = world
+        .driver
+        .find_element(By::ClassName("sign-up"))
+        .await
+        .unwrap();
     signup_button.click().await.unwrap();
 }
 
@@ -15,26 +19,47 @@ async fn fill_in_sign_up_form(world: &mut TestWorld) {
     let email_field = world.driver.find_element(By::Name("email")).await.unwrap();
     email_field.send_keys("test@email.com").await.unwrap();
 
-    let password_field = world.driver.find_element(By::Name("password")).await.unwrap();
+    let password_field = world
+        .driver
+        .find_element(By::Name("password"))
+        .await
+        .unwrap();
     password_field.send_keys("x,W-4,jfn").await.unwrap();
-    let create_account_button = world.driver.find_element(By::ClassName("create_account_btn")).await.unwrap();
+    let create_account_button = world
+        .driver
+        .find_element(By::ClassName("create_account_btn"))
+        .await
+        .unwrap();
     create_account_button.click().await.unwrap();
 }
 
-#[when(expr = "I fill in a valid email with value of '{word}' and an invalid password with value of {word}")]
+#[when(
+    expr = "I fill in a valid email with value of '{word}' and an invalid password with value of {word}"
+)]
 async fn fill_in_invalid_password(world: &mut TestWorld, email: String, invalid_password: String) {
     let email_field = world.driver.find_element(By::Name("email")).await.unwrap();
     email_field.send_keys(email).await.unwrap();
 
-    let password_field = world.driver.find_element(By::Name("password")).await.unwrap();
+    let password_field = world
+        .driver
+        .find_element(By::Name("password"))
+        .await
+        .unwrap();
     password_field.send_keys(invalid_password).await.unwrap();
-    let create_account_button = world.driver.find_element(By::ClassName("create_account_btn")).await.unwrap();
+    let create_account_button = world
+        .driver
+        .find_element(By::ClassName("create_account_btn"))
+        .await
+        .unwrap();
     create_account_button.click().await.unwrap();
 }
 
 #[then("I should see the sign up page")]
 async fn see_sign_up_page(world: &mut TestWorld) {
-  assert_eq!(world.driver.current_url().await.unwrap(), "http://localhost:17002/accounts/register/");
+    assert_eq!(
+        world.driver.current_url().await.unwrap(),
+        "http://localhost:17002/accounts/register/"
+    );
 }
 
 #[then("I should see that my account has been created")]
@@ -46,7 +71,11 @@ async fn see_my_account_created(world: &mut TestWorld) {
 
 #[then(regex = r"^I should see the error '([\w\s?]+)'$")]
 async fn see_error_message(world: &mut TestWorld, message: String) {
-    let errors_element = world.driver.find_element(By::ClassName("error")).await.unwrap();
+    let errors_element = world
+        .driver
+        .find_element(By::ClassName("error"))
+        .await
+        .unwrap();
     let errors_message = errors_element.text().await.unwrap();
     assert!(errors_message.contains(&message));
     world.close_browser().await;
