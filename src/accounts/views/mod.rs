2654ccef5d0d2b4f48d632deb2756c0c4f7ae7e3
ea-open-source <open-source@eastagile.com>
2022-04-18 10:10:48 +0700

    [f] 931 - add google signout
    [#181708931] https://www.pivotaltracker.com/story/show/181708931

--- src/accounts/views/mod.rs
+++ src/accounts/views/mod.rs
@@ -1,6 +1,7 @@
 //!  Views for user auth.
 
 use jelly::actix_session::UserSession;
+use jelly::actix_web::http::header;
 use jelly::prelude::*;
 use jelly::Result;
 
@@ -12,5 +13,9 @@ pub mod verify;
 
 pub async fn logout(request: HttpRequest) -> Result<HttpResponse> {
     request.get_session().clear();
-    request.redirect("/")
+    return Ok(HttpResponse::Found()
+        .header(header::SET_COOKIE, "sign_out=true; Path=/; Max-Age=10")
+        .header(header::LOCATION, "/accounts/register/")
+        .finish()
+    );
 }
