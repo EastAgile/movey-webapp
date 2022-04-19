4167629df03b1d668a22b68e6674c32eed14d8ee
ea-open-source <open-source@eastagile.com>
2022-04-19 16:24:54 +0700

    [f] 759 - Add api search package
    [#181907759]https://www.pivotaltracker.com/story/show/181907759

---
+++ migrations/2022-04-19-034806_add_function_and_trigger_to_update_tsv_packages/up.sql
@@ -0,0 +1,12 @@
+DROP FUNCTION if EXISTS update_tsv();
+
+CREATE FUNCTION update_tsv() RETURNS trigger AS $emp_stamp$
+  BEGIN
+	new.tsv := to_tsvector('english', new.name);
+    RETURN NEW;
+  END;
+$emp_stamp$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS packages_update ON packages;
+CREATE TRIGGER packages_update BEFORE INSERT OR UPDATE
+ON packages FOR EACH ROW EXECUTE PROCEDURE update_tsv();
