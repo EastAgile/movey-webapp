c68608f3e534cbe8840540b137ee8b50953fcd36
ea-open-source <open-source@eastagile.com>
2022-04-28 09:56:43 +0700

    [r] 763 - Use jQuery to calculate timestamp

--- static/js/search/packages.js
+++ static/js/search/packages.js
@@ -15,53 +15,7 @@ class Packages {
             const $elem = $(e.currentTarget);
             window.location = $elem.data('url') + '&sort_type=' + $elem.val();
         });
+        $('.right-wrapper time').timeago();
+        $('.right-wrapper time').prepend("Updated ");
     }
 }
-
-
-function sec_to_ymdhms(sec) {
-    let years_diff = Math.floor(sec / 31536000);
-    let months_diff = Math.floor((sec % 31536000) / 2628000);
-    let days_diff = Math.floor(((sec % 31536000) % 2628000) / 86400);
-    let hours_diff = Math.floor((((sec % 31536000) % 2628000) % 86400) / 3600);
-    let mins_diff = Math.floor(((((sec % 31536000) % 2628000) % 86400) % 3600) / 60);
-    let secs_diff = ((((sec % 31536000) % 2628000) % 86400) % 3600) % 60;
-    return {
-        year: years_diff,
-        month: months_diff,
-        day: days_diff,
-        hour: hours_diff,
-        minute: mins_diff,
-        second: secs_diff
-    }
-}
-
-function displayUpdateTime(el) {
-    let $timestamp = $(el);
-    let last_update = Date.parse($timestamp.data('last-update'));
-    let diff = Math.floor((Date.now() - last_update) / 1000);
-    diff = sec_to_ymdhms(diff);
-    diff_text = ""
-    
-
-    if (diff.year){
-        diff_text = diff.year.toString() + " years";
-    }
-    else if (diff.month) {
-        diff_text = diff.month.toString() + " months";
-    }
-    else if (diff.day) {
-        diff_text = diff.day.toString() + " days";
-    }
-    else if (diff.hour) {
-        diff_text = diff.hour.toString() + " hours";
-    }
-    else if (diff.minute) {
-        diff_text = diff.minute.toString() + " minutes";
-    }
-    else {
-        diff_text = diff.second.toString() + " seconds";
-    }
-    $timestamp.children('span').text("Updated " + diff_text + " ago");
-}
-
