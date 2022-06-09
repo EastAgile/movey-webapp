6d5d85856169c064cedad90ee2e3f05dc5991212
ea-open-source <open-source@eastagile.com>
2022-06-09 09:42:51 +0700

    Display package size in b, kb, mb where applicable

--- static/js/packages/show.js
+++ static/js/packages/show.js
@@ -2,6 +2,7 @@ class PackageShow {
     constructor() {
         this.readmeElement = $('.package-readme-content');
         this.instructionElement = $('.package-install-instruction');
+        this.packageSizeElement = $('.package-size');
         this.init();
     }
 
@@ -16,5 +17,17 @@ class PackageShow {
                 $('.copy-tooltip').hide();
             }, 400);
         });
+
+        this.packageSizeElement.text(this.niceBytes(this.packageSizeElement.data("value")));
+    }
+
+    // https://www.codegrepper.com/code-examples/javascript/kb+to+mb+to+gb+jquery
+    niceBytes(x) {
+        const units = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        let l = 0, n = parseInt(x, 10) || 0;
+        while(n >= 1024 && ++l){
+            n = n/1024;
+        }
+        return(n.toFixed(n < 10 && l > 0 ? 1 : 0) + ' ' + units[l]);
     }
 }
