ac9c907c53fab5d0a1fe6fb4b10033c48cbf34d9
ea-open-source <open-source@eastagile.com>
2022-06-10 11:00:21 +0700

    [c] - fix homepage sign in

--- static/js/header.js
+++ static/js/header.js
@@ -1,28 +1,16 @@
 class Header {
     constructor() {
-        this.menu = $('.menu-nav');
         this.navLinks = $('.nav-links');
-        this.signInButton = $('.sign-in');
 
         this.init();
     }
 
     init() {
-        this.menu.click(() => {
-            if (this.navLinks.css('left') === '0px') {
-                this.navLinks.css('left', '-300px');
-                this.signInButton.css('left', '-300px')
-            } else {
-                this.navLinks.css('left', '0px');
-                this.signInButton.css('left', '0px')
-            }
-        })
 
         $(document).click(e => {
             let clicked = e.target.className;
-            if(clicked !== 'nav-links' && clicked !== 'menu-nav') {
+            if(clicked !== 'nav-links') {
                 this.navLinks.css('left', '-300px');
-                this.signInButton.css('left', '-300px')
             }
         });
     }
