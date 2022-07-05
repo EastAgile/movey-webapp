a1d87603579119842dfeb6049ce1502a84b26961
ea-open-source <open-source@eastagile.com>
2022-07-05 15:09:58 +0700

    [b] 490 -  fix search bar undefined display
    182631490 - https://www.pivotaltracker.com/story/show/182631490

--- static/js/autocomplete/autocomplete.js
+++ static/js/autocomplete/autocomplete.js
@@ -116,7 +116,8 @@ class AutoComplete {
   }
 
   getOptionValue(suggestion) {
-    return typeof suggestion === "object" ? suggestion?.option : suggestion;
+    return typeof suggestion === "object" ? Object.values(suggestions) : suggestion;
+    // return typeof suggestion === "object" ? suggestion?.option : suggestion;
   }
 
   getSuggestionNode(id) {
@@ -157,12 +158,11 @@ class AutoComplete {
     }
 
     event.preventDefault();
+
     if (this.currentChoiceIndex === -1) {
       input.value = this.inputValue;
     } else {
-      input.value = this.getOptionValue(
-        this.suggestions[this.currentChoiceIndex]
-      );
+      input.value = this.suggestions[this.currentChoiceIndex][0];
     }
 
     const currentChoice = this.getSuggestionNode(this.currentChoiceIndex);
