b45ed6a7ea746bd9f87b0bea2af7e53cd30f6388
ea-open-source <open-source@eastagile.com>
2022-07-22 11:38:03 +0700

    [f] 382 - refactor `github_service.rs` and use default branch
    - Refactor `github_service.rs` to clarify meaning for lengthy logic
    - Use repo's default branch in the url instead of lazily using master
    
    [#182730382](https://www.pivotaltracker.com/story/show/182730382)

--- src/github_service.rs
+++ src/github_service.rs
@@ -1,10 +1,10 @@
 use jelly::error::Error;
+use jelly::error::Error::Generic;
 use reqwest::blocking::{multipart, Response};
 use reqwest::header;
 use serde::Deserialize;
 use std::env;
 use std::hash::{Hash, Hasher};
-use jelly::error::Error::Generic;
 
 #[derive(Deserialize)]
 struct MoveToml {
@@ -48,7 +48,7 @@ pub struct GithubRepoInfo {
     pub default_branch: String,
 }
 
-pub static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"), );
+pub static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 #[cfg(test)]
 use mockall::{automock, predicate::*};
@@ -64,7 +64,12 @@ impl GithubService {
 
 #[cfg_attr(test, automock)]
 impl GithubService {
-    pub fn fetch_repo_data(&self, repo_url: &String, path: Option<String>, mut rev: Option<String>) -> Result<GithubRepoData, Error> {
+    pub fn fetch_repo_data(
+        &self,
+        repo_url: &String,
+        path: Option<String>,
+        mut rev: Option<String>,
+    ) -> Result<GithubRepoData, Error> {
         let mut github_info = get_repo_description_and_size(&repo_url)?;
         if github_info.default_branch.is_empty() {
             github_info.default_branch = "master".to_string();
@@ -74,102 +79,29 @@ impl GithubService {
             if let Ok(sha) = get_repo_latest_commit_sha(&repo_url) {
                 rev = Some(sha)
             } else {
-                rev = Some(github_info.default_branch);
+                rev = Some(github_info.default_branch.clone());
             }
         }
         let rev = rev.unwrap();
 
-        let readme_url = format!(
-            "{}/{}/README.md",
-            repo_url.replace("https://github.com", "https://raw.githubusercontent.com"),
-            rev
-        );
-        let mut readme_content = "".to_string();
-        let response = call_github_api(&readme_url)?;
-        if response.status() != StatusCode::NOT_FOUND {
-            match call_github_api(&readme_url)?.text() {
-                Ok(content) => {
-                    // generate description from readme if not existed
-                    if github_info.description.is_none() {
-                        let mut description = call_deep_ai_api(content.clone())?;
-                        if description.len() > 400 {
-                            let deepai_summary = call_deep_ai_api(description.clone())?;
-                            if deepai_summary.len() > 0 {
-                                description = deepai_summary;
-                            }
-                        }
-                        description = strip_markdown::strip_markdown(&description);
+        let readme_content = get_repo_readme_content(repo_url, &rev, &mut github_info)?;
+        let move_toml = get_move_toml_content(repo_url, &rev, path)?;
 
-                        if description.is_empty() {
-                            description = strip_markdown::strip_markdown(&content);
-                        }
-                        if description.len() > 400 {
-                            description = description[0..400].to_string();
-                        }
-                        github_info.description = Some(description);
-                    }
-                    readme_content = content
-                }
-                _ => {
-                    warn!("Error getting README.md content. url: {}", readme_url);
-                }
-            }
-        }
-
-        let move_url = match path {
-            Some(path) => {
-                format!("{}/{}", readme_url.replace("/README.md", ""), path)
-            }
-            None => {
-                readme_url.replace("README.md", "Move.toml")
-            }
-        };
-
-        let mut move_toml_content = "".to_string();
-        match call_github_api(&move_url)?.text() {
-            Ok(content) => {
-                move_toml_content = content
-            }
-            Err(error) => {
-                error!(
-                        "Error getting Move.toml content. url: {:?}, error: {}",
-                        move_url, error
-                    );
-            }
-        };
-
-        match toml::from_str::<MoveToml>(&move_toml_content) {
-            Ok(move_toml) => Ok(GithubRepoData {
-                name: move_toml.package.name,
-                version: move_toml.package.version,
-                readme_content,
-                description: github_info.description.unwrap_or("".to_string()),
-                size: github_info.size,
-                url: String::from(""),
-                rev,
-            }),
-            Err(error) => {
-                warn!(
-                    "Invalid Move.toml url: {}, content: {}, error: {}",
-                    &move_url, &move_toml_content, &error
-                );
-                Ok(GithubRepoData {
-                    name: String::from(""),
-                    version: String::from(""),
-                    readme_content,
-                    description: github_info.description.unwrap_or("".to_string()),
-                    size: github_info.size,
-                    url: String::from(""),
-                    rev,
-                })
-            }
-        }
+        Ok(GithubRepoData {
+            name: move_toml.package.name,
+            version: move_toml.package.version,
+            readme_content,
+            description: github_info.description.unwrap_or("".to_string()),
+            size: github_info.size,
+            // this field is overwritten in the crawler, modified this to save default branch
+            url: github_info.default_branch,
+            rev,
+        })
     }
 }
 
 fn call_github_api(url: &str) -> Result<Response, Error> {
-    let access_token = env::var("GITHUB_ACCESS_TOKEN")
-        .expect("Unable to pull GITHUB_ACCESS_TOKEN");
+    let access_token = env::var("GITHUB_ACCESS_TOKEN").expect("Unable to pull GITHUB_ACCESS_TOKEN");
     let client = reqwest::blocking::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()?;
@@ -181,13 +113,11 @@ fn call_github_api(url: &str) -> Result<Response, Error> {
 }
 
 fn call_deep_ai_api(content: String) -> Result<String, Error> {
-    let access_token = env::var("DEEP_AI_API_KEY")
-        .expect("Unable to pull DEEP_AI_API_KEY");
+    let access_token = env::var("DEEP_AI_API_KEY").expect("Unable to pull DEEP_AI_API_KEY");
     let client = reqwest::blocking::Client::builder()
         .user_agent(APP_USER_AGENT)
         .build()?;
-    let form = multipart::Form::new()
-        .text("text", content);
+    let form = multipart::Form::new().text("text", content);
     let response = client
         .post("https://api.deepai.org/api/summarization")
         .header("api-key", access_token)
@@ -224,9 +154,9 @@ fn get_repo_description_and_size(repo_url: &str) -> Result<GithubRepoInfo, Error
         Ok(info) => Ok(info),
         Err(error) => {
             error!(
-                    "Error getting repo description and size. url: {:?}, error: {}",
-                    url, error
-                );
+                "Error getting repo description and size. url: {:?}, error: {}",
+                url, error
+            );
             Ok(Default::default())
         }
     }
@@ -244,17 +174,106 @@ fn get_repo_latest_commit_sha(repo_url: &str) -> Result<String, Error> {
         Ok(info) if !info.is_empty() => Ok(info.get(0).unwrap().sha.clone()),
         Ok(_) => {
             error!(
-                    "Error getting repo commit. url: {:?}, error: Empty response",
-                    url
-                );
-            Err(Generic(format!("Error getting repo commit. url: {:?}, error: Empty response", url)))
+                "Error getting repo commit. url: {:?}, error: Empty response",
+                url
+            );
+            Err(Generic(format!(
+                "Error getting repo commit. url: {:?}, error: Empty response",
+                url
+            )))
         }
         Err(error) => {
             error!(
-                    "Error getting repo commit. url: {:?}, error: {}",
-                    url, error
-                );
-            Err(Generic(format!("Error getting repo commit. url: {:?}, error: {}", url, error)))
+                "Error getting repo commit. url: {:?}, error: {}",
+                url, error
+            );
+            Err(Generic(format!(
+                "Error getting repo commit. url: {:?}, error: {}",
+                url, error
+            )))
+        }
+    }
+}
+
+fn get_repo_readme_content(
+    repo_url: &str,
+    rev: &str,
+    github_info: &mut GithubRepoInfo,
+) -> Result<String, Error> {
+    let readme_url = format!(
+        "{}/{}/README.md",
+        repo_url.replace("https://github.com", "https://raw.githubusercontent.com"),
+        rev
+    );
+    let mut readme_content = "".to_string();
+    let response = call_github_api(&readme_url)?;
+    if response.status() != StatusCode::NOT_FOUND {
+        match call_github_api(&readme_url)?.text() {
+            Ok(content) => {
+                // generate description from readme if not existed
+                if github_info.description.is_none() {
+                    let mut description = call_deep_ai_api(content.clone())?;
+                    if description.len() > 400 {
+                        let deepai_summary = call_deep_ai_api(description.clone())?;
+                        if deepai_summary.len() > 0 {
+                            description = deepai_summary;
+                        }
+                    }
+                    description = strip_markdown::strip_markdown(&description);
+
+                    if description.is_empty() {
+                        description = strip_markdown::strip_markdown(&content);
+                    }
+                    if description.len() > 400 {
+                        description = description[0..400].to_string();
+                    }
+                    github_info.description = Some(description);
+                }
+                readme_content = content
+            }
+            _ => {
+                warn!("Error getting README.md content. url: {}", readme_url);
+            }
+        }
+    }
+    Ok(readme_content)
+}
+
+fn get_move_toml_content(
+    repo_url: &str,
+    rev: &str,
+    path: Option<String>,
+) -> Result<MoveToml, Error> {
+    let move_url = format!(
+        "{}/{}/{}",
+        repo_url.replace("https://github.com", "https://raw.githubusercontent.com"),
+        rev,
+        path.unwrap_or_else(|| String::from("Move.toml"))
+    );
+
+    let mut move_toml_content = "".to_string();
+    match call_github_api(&move_url)?.text() {
+        Ok(content) => move_toml_content = content,
+        Err(error) => {
+            error!(
+                "Error getting Move.toml content. url: {:?}, error: {}",
+                move_url, error
+            );
+        }
+    };
+    match toml::from_str::<MoveToml>(&move_toml_content) {
+        Ok(move_toml) => Ok(move_toml),
+        Err(error) => {
+            warn!(
+                "Invalid Move.toml url: {}, content: {}, error: {}",
+                &move_url, &move_toml_content, &error
+            );
+            Ok(MoveToml {
+                package: PackageToml {
+                    name: String::from(""),
+                    version: String::from(""),
+                },
+            })
         }
     }
 }
