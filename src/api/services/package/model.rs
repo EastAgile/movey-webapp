2045cf7bceb642787a5ffb9cd7360b621db52c98
ea-open-source <open-source@eastagile.com>
2022-07-25 15:29:55 +0700

    [f] - Refactor package versions and downloads API

---
+++ src/api/services/package/model.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct PackageBadgeRespond {
+    package_name: String,
+    total_download_counts: i32,
+    latest_version: String,
+    versions: Vec<PackageBadgeVersion>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PackageBadgeVersion {
+    versions: String,
+    download_counts: i32,
+}
+
+impl From<Vec<(String, i32, String, i32)>> for PackageBadgeRespond {
+    fn from(tuples: Vec<(String, i32, String, i32)>) -> Self {
+        let mut max_version: String = tuples[0].2.to_string();
+        let mut shield_respond = PackageBadgeRespond {
+            package_name: tuples[0].0.to_string(),
+            total_download_counts: tuples[0].1,
+            latest_version: "".to_string(),
+            versions: vec![],
+        };
+        for record in tuples {
+            let version = PackageBadgeVersion {
+                versions: record.2.to_string(),
+                download_counts: record.3,
+            };
+            if (record.2.to_string() > max_version) {
+                max_version = record.2.to_string()
+            }
+            shield_respond.versions.push(version)
+        }
+        shield_respond.latest_version = max_version;
+        return shield_respond;
+    }
+}
