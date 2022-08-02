80637dff87a4cf4a96714b43e92183eaf4e45b69
ea-open-source <open-source@eastagile.com>
2022-08-02 11:18:17 +0700

    [f] 227A - Add unit test to owner_invitation,
    [#182803635](https://www.pivotaltracker.com/story/show/182803635)

--- src/package_collaborators/models/owner_invitation/tests.rs
+++ src/package_collaborators/models/owner_invitation/tests.rs
@@ -1,12 +1,14 @@
+use std::env;
 use crate::package_collaborators::models::owner_invitation::OwnerInvitation;
 use crate::packages::Package;
 use crate::test::{DatabaseTestContext, DB_POOL};
 use crate::utils::tests::setup_user;
 use jelly::prelude::*;
+use crate::utils::token::TOKEN_LENGTH;
 
-async fn setup_user_package() -> (i32, i32, i32) {
-    let uid1 = setup_user(None, None).await;
-    let uid2 = setup_user(Some("email1@mail.com".to_string()), None).await;
+async fn setup_invitation() -> OwnerInvitation {
+    let invited_uid = setup_user(None, None).await;
+    let invited_by_uid = setup_user(Some("email1@mail.com".to_string()), None).await;
     let pid = Package::create_test_package(
         &"package1".to_string(),
         &"".to_string(),
@@ -16,23 +18,20 @@ async fn setup_user_package() -> (i32, i32, i32) {
         &"".to_string(),
         -1,
         -1,
-        Some(uid1),
+        Some(invited_uid),
         &DB_POOL,
     )
     .await
     .unwrap();
-    (uid1, uid2, pid)
+    OwnerInvitation::create(invited_uid, invited_by_uid, pid, &DB_POOL.get().unwrap()).unwrap()
 }
 #[actix_rt::test]
 async fn find_by_token_works() {
     crate::test::init();
     let _ctx = DatabaseTestContext::new();
+    let conn = DB_POOL.get().unwrap();
 
-    let (uid1, uid2, pid) = setup_user_package().await;
-    let db = &DB_POOL;
-    let conn = db.get().unwrap();
-    let owner_invitation1 = OwnerInvitation::create(uid1, uid2, pid, &conn).unwrap();
-
+    let owner_invitation1 = setup_invitation().await;
     let owner_invitation2 =
         OwnerInvitation::find_by_token(&owner_invitation1.token, &conn).unwrap();
     assert_eq!(owner_invitation1, owner_invitation2);
@@ -48,15 +47,20 @@ async fn find_by_token_works() {
 async fn find_by_id_works() {
     crate::test::init();
     let _ctx = DatabaseTestContext::new();
+    let conn = DB_POOL.get().unwrap();
 
-    let (uid1, uid2, pid) = setup_user_package().await;
-    let db = &DB_POOL;
-    let conn = db.get().unwrap();
-    let owner_invitation1 = OwnerInvitation::create(uid1, uid2, pid, &conn).unwrap();
-
-    let owner_invitation2 = OwnerInvitation::find_by_id(uid1, pid, &conn).unwrap();
+    let owner_invitation1 = setup_invitation().await;
+    let owner_invitation2 = OwnerInvitation::find_by_id(
+        owner_invitation1.invited_user_id,
+        owner_invitation1.package_id,
+        &conn
+    ).unwrap();
     assert_eq!(owner_invitation1, owner_invitation2);
-    let not_found = OwnerInvitation::find_by_id(uid2, pid, &conn);
+    let not_found = OwnerInvitation::find_by_id(
+        owner_invitation1.invited_by_user_id,
+        owner_invitation1.package_id,
+        &conn
+    );
     assert!(not_found.is_err());
     if let Err(Error::Database(diesel::NotFound)) = not_found {
     } else {
@@ -68,12 +72,9 @@ async fn find_by_id_works() {
 async fn delete_works() {
     crate::test::init();
     let _ctx = DatabaseTestContext::new();
+    let conn = DB_POOL.get().unwrap();
 
-    let (uid1, uid2, pid) = setup_user_package().await;
-    let db = &DB_POOL;
-    let conn = db.get().unwrap();
-    let owner_invitation = OwnerInvitation::create(uid1, uid2, pid, &conn).unwrap();
-
+    let owner_invitation = setup_invitation().await;
     owner_invitation.delete(&conn).unwrap();
     let not_found = OwnerInvitation::find_by_token(&owner_invitation.token, &conn);
     assert!(not_found.is_err());
@@ -81,4 +82,97 @@ async fn delete_works() {
     } else {
         panic!()
     }
+}
+
+#[actix_rt::test]
+async fn is_expired_works() {
+    crate::test::init();
+    let _ctx = DatabaseTestContext::new();
+
+    let owner_invitation = setup_invitation().await;
+    env::set_var("OWNERSHIP_INVITATIONS_EXPIRATION_DAYS", "1");
+    assert!(!owner_invitation.is_expired());
+
+    env::set_var("OWNERSHIP_INVITATIONS_EXPIRATION_DAYS", "0");
+    assert!(owner_invitation.is_expired());
+}
+
+#[actix_rt::test]
+#[should_panic]
+async fn is_expired_panics_if_expiration_days_is_less_than_0() {
+    crate::test::init();
+    let _ctx = DatabaseTestContext::new();
+
+    let owner_invitation = setup_invitation().await;
+    env::set_var("OWNERSHIP_INVITATIONS_EXPIRATION_DAYS", "-1");
+    owner_invitation.is_expired();
+}
+
+#[actix_rt::test]
+#[should_panic]
+async fn is_expired_panics_if_expiration_days_is_not_an_integer() {
+    crate::test::init();
+    let _ctx = DatabaseTestContext::new();
+
+    let owner_invitation = setup_invitation().await;
+    env::set_var("OWNERSHIP_INVITATIONS_EXPIRATION_DAYS", "invalid-integer");
+    owner_invitation.is_expired();
+}
+
+#[actix_rt::test]
+async fn create_works() {
+    crate::test::init();
+    let _ctx = DatabaseTestContext::new();
+    let conn = DB_POOL.get().unwrap();
+
+    let owner_invitation1 = setup_invitation().await;
+    let owner_invitation2 = OwnerInvitation::find_by_id(
+        owner_invitation1.invited_user_id,
+        owner_invitation1.package_id,
+        &conn
+    ).unwrap();
+    assert_eq!(owner_invitation1, owner_invitation2);
+    assert_eq!(owner_invitation1.token.len(), TOKEN_LENGTH)
+}
+
+#[actix_rt::test]
+async fn create_new_invitation_if_existing_one_is_expired() {
+    crate::test::init();
+    let _ctx = DatabaseTestContext::new();
+    let conn = DB_POOL.get().unwrap();
+
+    let owner_invitation = setup_invitation().await;
+    let token = owner_invitation.token;
+    let created_at = owner_invitation.created_at;
+
+    env::set_var("OWNERSHIP_INVITATIONS_EXPIRATION_DAYS", "0");
+    let owner_invitation = OwnerInvitation::create(
+        owner_invitation.invited_user_id,
+        owner_invitation.invited_by_user_id,
+        owner_invitation.package_id,
+        &conn
+    ).unwrap();
+    assert_ne!(token, owner_invitation.token);
+    assert_ne!(created_at, owner_invitation.created_at);
+}
+
+#[actix_rt::test]
+async fn not_create_new_invitation_if_it_already_exists() {
+    crate::test::init();
+    let _ctx = DatabaseTestContext::new();
+    let conn = DB_POOL.get().unwrap();
+
+    let owner_invitation = setup_invitation().await;
+    let token = owner_invitation.token;
+    let created_at = owner_invitation.created_at;
+
+    env::set_var("OWNERSHIP_INVITATIONS_EXPIRATION_DAYS", "1");
+    let owner_invitation = OwnerInvitation::create(
+        owner_invitation.invited_user_id,
+        owner_invitation.invited_by_user_id,
+        owner_invitation.package_id,
+        &conn
+    ).unwrap();
+    assert_eq!(token, owner_invitation.token);
+    assert_eq!(created_at, owner_invitation.created_at);
 }
